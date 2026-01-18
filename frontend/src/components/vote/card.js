d297310fcc113ab24cdbbd0d5e30e27812ae89d6
staanaeleina <eleinastaana06@gmail.com>
2026-01-18 22:28:36 +0800

    updated cards

--- frontend/src/components/vote/card.js
+++ frontend/src/components/vote/card.js
@@ -2,15 +2,16 @@
 
 import styles from "../vote/component.module.css";
 
-export default function Card({ name, image }) {
+export default function Card({ name, image, status_badge }) {
   return `
-    <div class="${styles['card']}">
-        <div class="${styles['card_content']}">
-            <div class="${styles['card_image']}" style="background-image: url('${image}')"></div>
-            <div class="${styles['card_text']}">
-                <p class="${styles['card_title']}">${name}</p>
-            </div>
-        </div>
+    <div class="${styles["card"]}">
+      <div class="${styles["status_badge"]}">${status_badge}</div>
+      <div class="${styles["card_content"]}">
+          <div class="${styles["card_image"]}" style="background-image: url('${image}')"></div>
+          <div class="${styles["card_text"]}">
+              <p class="${styles["card_title"]}">${name}</p>
+          </div>
+      </div>
     </div>
   `;
 }
