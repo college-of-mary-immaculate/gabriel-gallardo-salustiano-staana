092c2a58b94aaad77f62ea06c7ee8e368c9d73db
staanaeleina <eleinastaana06@gmail.com>
2026-01-19 00:28:43 +0800

    updated car component

--- frontend/src/components/vote/card.js
+++ frontend/src/components/vote/card.js
@@ -2,7 +2,7 @@
 
 import styles from "../vote/component.module.css";
 
-export default function Card({ name, image, status_badge }) {
+export default function Card({ name, image, status_badge, total_votes }) {
   return `
     <div class="${styles["card"]}">
       <div class="${styles["status_badge"]}">${status_badge}</div>
@@ -10,6 +10,13 @@ export default function Card({ name, image, status_badge }) {
           <div class="${styles["card_image"]}" style="background-image: url('${image}')"></div>
           <div class="${styles["card_text"]}">
               <p class="${styles["card_title"]}">${name}</p>
+              <p class="${styles["card_total_votes"]}">${total_votes} votes</p>
+              <div class="${styles["card_progress_container"]}">
+                <p class="${styles["card_progress_label"]}">Chance of Winning: 42%</p>
+                <div class="${styles["card_progress_bar"]}">
+                    <div class="${styles["card_progress_fill"]}"></div>
+                </div>
+              </div>
           </div>
       </div>
     </div>
--- frontend/src/components/vote/event.js
+++ frontend/src/components/vote/event.js
@@ -6,42 +6,49 @@ export const candidates = [
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1767584015/v8_cdshyj.avif",
     status_badge: "1",
+    total_votes: "3450",
   },
   {
     name: "Jane Smith",
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1768321566/Rectangle_39_2_nj4cvn.png",
     status_badge: "2",
+    total_votes: "3450",
   },
   {
     name: "Bob Johnson",
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1768321566/Rectangle_39_5_ggem1u.png",
     status_badge: "5",
+    total_votes: "3450",
   },
   {
     name: "Alice Williams",
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1768321566/Rectangle_39_1_ivzpoy.png",
     status_badge: "3",
+    total_votes: "3450",
   },
   {
     name: "Charlie Brown",
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1768321566/Rectangle_39_3_sn1toa.png",
     status_badge: "4",
+    total_votes: "3450",
   },
   {
     name: "Diana Prince",
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1768321566/Rectangle_39_rhruv8.png",
     status_badge: "6",
+    total_votes: "3450",
   },
   {
     name: "Eve Davis",
     image:
       "https://res.cloudinary.com/hamsters-api/image/upload/v1768321566/Rectangle_39_4_oye3yj.png",
     status_badge: "7",
+    total_votes: "3450",
   },
 ];
 
