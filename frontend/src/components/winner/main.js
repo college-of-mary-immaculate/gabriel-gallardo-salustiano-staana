d617eabacfa28743e9f069c48eaee192ab81656f
lykhasalustinao <lykhasalustiano@gmail.com>
2026-01-20 14:07:47 +0800

    :coffin:

--- frontend/src/components/winner/main.js
+++ frontend/src/components/winner/main.js
@@ -15,80 +15,112 @@ export default function Winner(root) {
           src="https://res.cloudinary.com/deogcjil5/image/upload/v1768886062/Screenshot_2026-01-20_130819-removebg-preview_1_rzucf1.png" 
           alt="Juan Cruz" 
           class="${styles['avatar']}">
+
         <div class="${styles['card-info']}">
           <div class="${styles['name-role']}">
             <span class="${styles['name']}">Juan Cruz</span>
-            <span class="${styles['role president']}">President</span>
+            <span class="${styles['role']}">President</span>
           </div>
+
           <div class="${styles['votes']}">
+           <img 
+              src="https://res.cloudinary.com/deogcjil5/image/upload/v1768887078/Screenshot_2026-01-20_133026-removebg-preview_mdy3ma.png" 
+              alt="vote icon" 
+              class="${styles['vote-icon']}">
             <span class="${styles['vote-count']}">30,021 votes</span>
-            <div class="${styles['progress-bar']}">
-              <div class="${styles['progress president']}" style="width: 87%;"></div>
-            </div>
+            <span class="${styles['percent']}">87%</span>
+          </div>
+          
+          <div class="${styles['progress-bar']}">
           </div>
         </div>
-        <span class="${styles['percent']}">87%</span>
       </div>
+    </div>
 
+    <div class="${styles['cards']}">
       <div class="${styles['card']}">
         <img 
           src="https://res.cloudinary.com/deogcjil5/image/upload/v1768886062/Screenshot_2026-01-20_130819-removebg-preview_1_rzucf1.png" 
           alt="Juan Cruz" 
           class="${styles['avatar']}">
+
         <div class="${styles['card-info']}">
           <div class="${styles['name-role']}">
             <span class="${styles['name']}">Juan Cruz</span>
-            <span class="${styles['role vice-president']}">Vice President</span>
+            <span class="${styles['role']}">Vice President</span>
           </div>
+
           <div class="${styles['votes']}">
+           <img 
+              src="https://res.cloudinary.com/deogcjil5/image/upload/v1768887078/Screenshot_2026-01-20_133026-removebg-preview_mdy3ma.png" 
+              alt="vote icon" 
+              class="${styles['vote-icon']}">
             <span class="${styles['vote-count']}">30,021 votes</span>
-            <div class="${styles['progress-bar']}">
-              <div class="${styles['progress vice-president']}" style="width: 87%;"></div>
-            </div>
+            <span class="${styles['percent']}">87%</span>
+          </div>
+          
+          <div class="${styles['progress-bar']}">
           </div>
         </div>
-        <span class="${styles['percent']}">87%</span>
       </div>
+    </div>
 
+    <div class="${styles['cards']}">
       <div class="${styles['card']}">
         <img 
           src="https://res.cloudinary.com/deogcjil5/image/upload/v1768886062/Screenshot_2026-01-20_130819-removebg-preview_1_rzucf1.png" 
           alt="Juan Cruz" 
           class="${styles['avatar']}">
+
         <div class="${styles['card-info']}">
           <div class="${styles['name-role']}">
             <span class="${styles['name']}">Juan Cruz</span>
-            <span class="${styles['role secretary']}">Secretary</span>
+            <span class="${styles['role']}">Secretary</span>
           </div>
+
           <div class="${styles['votes']}">
+           <img 
+              src="https://res.cloudinary.com/deogcjil5/image/upload/v1768887078/Screenshot_2026-01-20_133026-removebg-preview_mdy3ma.png" 
+              alt="vote icon" 
+              class="${styles['vote-icon']}">
             <span class="${styles['vote-count']}">30,021 votes</span>
-            <div class="${styles['progress-bar']}">
-              <div class="${styles['progress secretary']}" style="width: 87%;"></div>
-            </div>
+            <span class="${styles['percent']}">87%</span>
+          </div>
+          
+          <div class="${styles['progress-bar']}">
           </div>
         </div>
-        <span class="${styles['percent']}">87%</span>
       </div>
+    </div>
 
+    <div class="${styles['cards']}">
       <div class="${styles['card']}">
         <img 
           src="https://res.cloudinary.com/deogcjil5/image/upload/v1768886062/Screenshot_2026-01-20_130819-removebg-preview_1_rzucf1.png" 
           alt="Juan Cruz" 
           class="${styles['avatar']}">
+
         <div class="${styles['card-info']}">
           <div class="${styles['name-role']}">
             <span class="${styles['name']}">Juan Cruz</span>
-            <span class="${styles['role senator']}">Senator</span>
+            <span class="${styles['role']}">Senator</span>
           </div>
+
           <div class="${styles['votes']}">
+           <img 
+              src="https://res.cloudinary.com/deogcjil5/image/upload/v1768887078/Screenshot_2026-01-20_133026-removebg-preview_mdy3ma.png" 
+              alt="vote icon" 
+              class="${styles['vote-icon']}">
             <span class="${styles['vote-count']}">30,021 votes</span>
-            <div class="${styles['progress-bar']}">
-              <div class="${styles['progress senator']}" style="width: 87%;"></div>
-            </div>
+            <span class="${styles['percent']}">87%</span>
+          </div>
+          
+          <div class="${styles['progress-bar']}">
           </div>
         </div>
-        <span class="${styles['percen']}t">87%</span>
       </div>
+    </div>
+
     </div>
   </div>
 </body>
