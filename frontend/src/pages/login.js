61d2cd53267c7f30e3d644064309d181d44f2163
staanaeleina <eleinastaana06@gmail.com>
2026-01-21 23:22:13 +0800

    deleted auth.js layout

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,10 +1,8 @@
-import AuthTemplate from "../layouts/auth.js";
+import Layout from "../layouts/default.js";
 import Main from "../components/login/main.js";
 
-
 export default function LoginPage() {
-  const { main } = AuthTemplate(this.root);
+  const { main } = Layout(this.root);
 
   Main(main);
-
 }
--- frontend/src/pages/pageNotFound.js
+++ frontend/src/pages/pageNotFound.js
@@ -1,9 +1,9 @@
-import AuthTemplate from "../layouts/auth.js";
+import Layout from "../layouts/default.js";
 import Main from "../components/pageNotFound/main.js";
 import Events from "../components/pageNotFound/event.js";
 
 export default function PageNotFound() {
-  const { main } = AuthTemplate(this.root);
+  const { main } = Layout(this.root);
 
   Main(main);
 
