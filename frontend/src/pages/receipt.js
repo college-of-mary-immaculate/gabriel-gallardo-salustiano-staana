fc137bdb9604867ec4fcfe59e8f5a7fb25d8dfa9
staanaeleina <eleinastaana06@gmail.com>
2026-01-21 22:32:21 +0800

    fixed footer

--- frontend/src/pages/receipt.js
+++ frontend/src/pages/receipt.js
@@ -1,12 +1,13 @@
-import Layout  from "../layouts/default.js";
+import Layout from "../layouts/default.js";
 import Main from "../components/receipt/main.js";
 import Header from "../components/header/header.js";
-
+import Footer from "../components/footer/footer.js";
 
 export default function ReceiptPage() {
-  const { main, header } = Layout(this.root);
+  const { main, header, footer } = Layout(this.root);
   Header(header);
   Main(main);
+  Footer(footer);
 
   Events();
 }
