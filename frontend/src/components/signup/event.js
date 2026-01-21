25d31d1922617445f7c002d3f84b8c54585fbaff
staanaeleina <eleinastaana06@gmail.com>
2026-01-22 06:45:47 +0800

    updated signup pages

--- frontend/src/components/signup/event.js
+++
@@ -1,8 +0,0 @@
-export default async function Events() {
-  window.addEventListener("load", async function () {
-    console.log('SignUp Page Event')
-  });
-  
-  const socket = io();
-  console.log(socket);
-}
