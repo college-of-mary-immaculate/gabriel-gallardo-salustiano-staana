2028dd820fb512ae59bd682d4495b69a0fa9bbff
Izyne Gallardo <izynehowiegallardo@gmail.com>
2026-01-21 20:41:26 +0800

    frontend
    adjusted API calls

--- frontend/src/utils/account.js
+++ frontend/src/utils/account.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getProfile() {
   try {
-    const response = await axios.get(`http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/v1/account`, {
+    const response = await axios.get(`http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/api/v1/account`, {
       headers: {
         "Content-type": "application/json",
         apikey: import.meta.env.VITE_API_KEY,
