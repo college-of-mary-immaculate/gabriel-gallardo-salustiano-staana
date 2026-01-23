e60f5a557ec83c7f04a3e43621a83fc6a7a05ed4
staanaeleina <eleinastaana06@gmail.com>
2026-01-23 23:06:28 +0800

    updated vote page mobile view

--- frontend/src/pages/vote.js
+++ frontend/src/pages/vote.js
@@ -1,6 +1,6 @@
-import VoteLayoutTemplate from "../layouts/voteLayout.js";
+import VoteLayoutTemplate from "../layouts/default.js";
 import Main from "../components/vote/main.js";
-import Sidebar from "../components/vote/sidebar.js";
+import Sidebar from "../components/sidebar/sidebar.js";
 import Header from "../components/header/header.js";
 
 export default function VotePage() {
